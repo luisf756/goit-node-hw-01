753da4de0582e6673652af96c8e4300311b8d095
luisf756 <luchof756@gmail.com>
2023-09-13 12:26:34 -0500

    add commander

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const contactsModule = require("./contacts.js");
 
-const argv = require("yargs").argv;
+//const argv = require("yargs").argv;
+// using comander
+const { Command } = require("commander");
 
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
+program.parse(process.argv);
+
+const argv = program.opts();
 // TODO: refactorizar
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
